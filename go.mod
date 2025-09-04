b9c0e23ad22c6fa641a54cc83c81c039f62a8303
Carl Holmberg <holmberg.carl.j@gmail.com>
2025-09-04 21:35:25 +0200

    4.1 tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cajoho99/httpfromtcp
 
 go 1.24.6
+
+require github.com/magiconair/properties v1.8.10
---
+++ internal/request/request_test.go
@@ -0,0 +1,11 @@
+package request
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestRequestLineParse(t *testing.T) {
+	assert.Equal(t, "CarlsProto", "CarlsProto")
+}
