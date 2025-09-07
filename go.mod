2c90a7828b1fe859c70067dd0ba21d8ae7f4a75c
Carl Holmberg <holmberg.carl.j@gmail.com>
2025-09-07 20:32:29 +0200

    4.2 tests

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/cajoho99/httpfromtcp
 
 go 1.24.6
 
-require github.com/magiconair/properties v1.8.10
+require github.com/stretchr/testify v1.11.1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,79 @@
 package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	str := string(b)
+
+	rawRqLine := strings.Split(str, "\r\n")
+	reqLine, err := parseRequestLine(rawRqLine[0])
+
+	if err != nil {
+		return nil, err
+	}
+
+	out := Request{reqLine}
+
+	return &out, nil
+}
+
+func parseRequestLine(s string) (RequestLine, error) {
+	parts := strings.Split(s, " ")
+
+	fmt.Println(len(parts))
+	if len(parts) != 3 {
+		return RequestLine{}, errors.New("Request-line does not contain the three specified parts")
+	}
+
+	// Verify method
+	method := parts[0]
+	if !isAlphabeticAndUppercase(method) {
+		return RequestLine{}, errors.New("Method is not valid")
+
+	}
+
+	target := parts[1]
+
+	// Verify version
+	versionFull := parts[2]
+	versionParts := strings.Split(versionFull, "/")
+	if versionParts[0] != "HTTP" || versionParts[1] != "1.1" {
+		return RequestLine{}, errors.New("Version of HTTP not supported. Only HTTP/1.1 is supported.")
+	}
+
+	return RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   versionParts[1],
+	}, nil
+
+}
+
+func isAlphabeticAndUppercase(s string) bool {
+	return !strings.ContainsFunc(s,
+		func(r rune) bool {
+			return !unicode.IsLetter(r) || !unicode.IsUpper(r)
+		})
+}
